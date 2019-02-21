cdbe84029f21fbf1733aee6455caddc690036889
Laura <laurabalasso96@gmail.com>
2019-02-21 10:17:00 +0100

    struct Node updated

--- C++/src/node.h
+++ C++/src/node.h
@@ -14,7 +14,20 @@ struct Node{
     std::pair<T1,T2> node_pair;
     Node<T1,T2>* left;
     Node<T1,T2>* right;
+    template <class t, class u>
+    friend std::ostream& operator<<(std::ostream& os, const Node<t,u>* node);
     ~Node(){};
 };
- 
+
+template <class t, class u>
+/**
+ overloading the operator<< in order to print key:value of a node
+ */
+std::ostream& operator<<(std::ostream& os, const Node<t,u>* node){
+    
+    os<<"( key: "<<node->node_pair.first<<", value: "<<node->node_pair.second<<" )";
+    return os;
+}
+
+
 #endif
