b9e4d88cb78617587fb72a98c9a23b9bffa2c744
Laura <laurabalasso96@gmail.com>
2019-02-21 10:38:32 +0100

    class BST updated

--- C++/src/bst.h
+++ C++/src/bst.h
@@ -22,6 +22,10 @@ class BST{
     int n_nodes;
     int height;
 
+    void insert(T1 key, T2 value,Node<T1,T2>* leaf,int level);
+    void recursiveInsert(Node<T1,T2>* leaf);
+    void print(Node<T1,T2>* leaf);
+    
     public:
 
     BST();
@@ -29,8 +33,13 @@ class BST{
     ~BST() noexcept;
     BST<T1,T2>& operator=(const BST<T1,T2>&& bst) noexcept;
 
+    
+    Node<T1,T2>*  begin();
+    const Node<T1,T2>* cbegin();
     Node<T1,T2>*  end();
     const Node<T1,T2>* cend();
+    void insert(T1 key,T2 value);
+    void print();
 
 };
 
@@ -100,6 +109,22 @@ BST<T1,T2>& BST<T1,T2>::operator=(const BST<T1,T2>&& bst) noexcept {
 
 template <typename T1, typename T2>
 /**
+ function that returns the pointer to the first node
+ */
+Node<T1,T2>* BST<T1,T2>::begin(){
+    return root;
+}
+
+template <typename T1, typename T2>
+/**
+ function that reutrn a const pointer to the first node
+ */
+const Node<T1,T2>* BST<T1,T2>::cbegin(){
+    return(const_cast<const Node<T1,T2>*>(root));
+}
+
+template <typename T1, typename T2>
+/**
     function that returns a pointer to the deepest node of the tree
 */
 Node<T1,T2>* BST<T1,T2>::end(){
@@ -112,6 +137,108 @@ template <typename T1, typename T2>
 */
 const Node<T1,T2>* BST<T1,T2>::cend(){
         return(const_cast<const Node<T1,T2>*>(tail));
+}
+
+template <typename T1, typename T2>
+/**
+ method that allows you to insert a node inside a binary search tree in a recursive way
+ */
+void BST<T1,T2>::insert(T1 key, T2 value,Node<T1,T2>* leaf, int level){
+    
+    if(key< leaf->node_pair.first){     //if the key is smaller than then the key of the actual node
+        
+        if(leaf->left!=nullptr){        //if the actual node has a left child
+            level++;
+            insert(key,value,(leaf->left),level);
+        }
+        else{                           //if the actual node doesn't have a left child
+            leaf->left=new Node<T1,T2>;
+            leaf->left->node_pair.first=key;
+            leaf->left->node_pair.second=value;
+            leaf->left->left=nullptr;
+            leaf->left->right=nullptr;
+            n_nodes++;
+            if(level>=height){
+                height=level;
+                tail = leaf->left;
+            }
+        }
+    }
+    else if(key>leaf->node_pair.first){     //if the key is bigger than then the key of the actual node
+        
+        if(leaf->right!=nullptr){           //if the actual node has a right child
+            level++;
+            insert(key,value,leaf->right,level);
+        }
+        else{                               //if the actual node doesn't have a right child
+            leaf->right=new Node<T1,T2>;
+            leaf->right->node_pair.first=key;
+            leaf->right->node_pair.second=value;
+            leaf->right->left=nullptr;
+            leaf->right->right=nullptr;
+            n_nodes++;
+            if(level>=height){
+                level=height;
+                tail = leaf->right;
+            }
+        }
+    }
+}
+template <typename T1, typename T2>
+/**
+ overloading of the insert method, in order to pass only two parameters to it: key and value of the new node.
+ */
+void BST<T1,T2>::insert(T1 key,T2 value){
+    
+    if(root == nullptr){           //if the tree is empty
+        root = new Node<T1,T2>{std::make_pair(key,value),nullptr,nullptr};
+        n_nodes++;
+        height++;
+    }
+    else{                          //if the tree is not empty
+        insert(key,value, begin(),this->height);
+    }
+    
+    
+}
+
+template <typename T1, typename T2>
+/**
+ method used to implement the copy semantic, that exploits the insert function.
+ */
+void BST<T1,T2>::recursiveInsert(Node<T1,T2>* leaf){
+    
+    if(leaf != nullptr){
+        insert(leaf->node_pair.first,leaf->node_pair.second);
+        recursiveInsert(leaf->left);
+        insert(leaf->node_pair.first,leaf->node_pair.second);
+        recursiveInsert(leaf->right);
+    }
+}
+
+template <typename T1, typename T2>
+/**
+ recursive method that prints the tree in-order
+ */
+void BST<T1,T2>::print(Node<T1,T2>* leaf){
+    
+    if(n_nodes==0){             //if the tree is empty
+        std::cout<<"empty tree"<<std::endl;
+    }
+    else if(leaf!=nullptr){     //if the tree is not empty
+        print(leaf->left);
+        std::cout<< leaf<<std::endl;
+        print(leaf->right);
+    }
+}
+
+template <typename T1, typename T2>
+/**
+ overloading of print method
+ */
+void BST<T1,T2>::print(){
+    
+    print(begin());
 }
 
 #endif
