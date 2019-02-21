67f65c6da82f9cab9e9d1d4b00dda78a2b943b7e
enricofallacara <enrico.fallacara@gmail.com>
2019-02-21 10:46:05 +0100

    Updated bst.h header

--- C++/src/bst.h
+++ C++/src/bst.h
@@ -25,10 +25,15 @@ class BST{
     void insert(T1 key, T2 value,Node<T1,T2>* leaf,int level);
     void recursiveInsert(Node<T1,T2>* leaf);
     void print(Node<T1,T2>* leaf);
+    void clear(Node<T1,T2>* leaf) noexcept;
+    void buildBalance(std::vector<std::pair<T1,T2>> &nodes,int start, int end);
+    void getVectorFromNodes(Node<T1,T2>* leaf,std::vector<std::pair<T1,T2>> &nodes);
+    Node<T1,T2>* find(T1 key, Node<T1,T2>* leaf);
     
     public:
 
     BST();
+    BST(T1 key, T2 value);
     BST(BST &&bst) noexcept;
     ~BST() noexcept;
     BST<T1,T2>& operator=(const BST<T1,T2>&& bst) noexcept;
@@ -40,6 +45,9 @@ class BST{
     const Node<T1,T2>* cend();
     void insert(T1 key,T2 value);
     void print();
+    void clear();
+    void balance();
+    Node<T1,T2>* find(T1 key);
 
 };
 
@@ -61,6 +69,15 @@ BST<T1,T2>::BST(){
     n_nodes = 0;
     height= 0;
 } 
+
+template <typename T1, typename T2>
+/**
+    constructor that allows you to create a binary search tree with the root node initialized
+*/
+BST<T1,T2>::BST(T1 key,T2 value){
+    BST();
+    insert(key,value);
+}
 
 template <typename T1, typename T2>
 /**
@@ -239,6 +256,102 @@ template <typename T1, typename T2>
 void BST<T1,T2>::print(){
     
     print(begin());
+}
+
+template <typename T1, typename T2>
+/**
+    recursive method that deletes the content of a binary search tree
+*/
+void BST<T1,T2>::clear(Node<T1,T2>* leaf) noexcept{
+        
+    if(leaf==nullptr)          //if the node is the root node of the binary search tree
+        return;
+    clear(leaf->left);
+    clear(leaf->right);
+    delete(leaf); 
+}
+
+template <typename T1, typename T2>
+/**
+    overloading of clear method
+*/
+void BST<T1,T2>::clear(){
+   
+    clear(begin());
+    n_nodes=0;
+    height=0;
+    root = nullptr;
+    tail = nullptr;
+}
+
+template <typename T1, typename T2>
+/**
+    method that stores all the nodes in-ored inside a std::vector, this is used to balance the binary search tree.
+*/
+void BST<T1,T2>::getVectorFromNodes(Node<T1,T2>* leaf,std::vector<std::pair<T1,T2>> &nodes){ 
+    
+    if(leaf==NULL){
+        return;
+    } 
+    else{
+        getVectorFromNodes(leaf->left,nodes);
+        nodes.push_back(leaf->node_pair);
+        getVectorFromNodes(leaf->right,nodes);
+    }
+}
+template <typename T1, typename T2>
+/**
+    recursive method that balances a binary search tree using: given a vector that contains all the nodes,
+    a pointer to the current node and two vector indexes.
+*/
+void BST<T1,T2>::buildBalance(std::vector<std::pair<T1,T2>> &nodes,int start, int end){
+    
+    if(start>end ) return;
+    int mid = (int)((start+end)/2);
+    insert(nodes[mid].first,nodes[mid].second);
+    buildBalance(nodes,start,mid-1);
+    buildBalance(nodes,mid+1,end);
+}
+template <typename T1, typename T2>
+/**
+    method that balances the tree, using the buildBalance and getVectorFromNodes methods
+*/
+void BST<T1,T2>::balance(){ 
+
+    std::vector<std::pair<T1,T2>> nodes;
+    getVectorFromNodes(root,nodes);
+    clear();
+    buildBalance(nodes,0,nodes.size()-1);
+}
+
+template <typename T1, typename T2>
+/**
+    function that find a specif node inside the binaray search tree and returns a pointer to that node,
+    if the node is not present in the tree, it returns a pointer to the last node.
+*/
+Node<T1,T2>* BST<T1,T2>::find(T1 key, Node<T1,T2>* leaf){
+
+    if(leaf==nullptr){                   //if the node is empty
+       // std::cout<<"key not found, it returns the pointer to the end node: ";
+        return tail;
+    }
+    if(leaf->node_pair.first==key){      //if the left child has the wanted key
+        return leaf;
+    }
+    if(leaf->node_pair.first < key){     //if the right child has the wanted key
+        return find(key,leaf->right);
+    }
+    else{                                //if the key is not found at the actual node                             
+        return find(key,leaf->left);
+    }
+}
+template <typename T1, typename T2>
+/**
+    overloading of the find function.
+*/
+Node<T1,T2>* BST<T1,T2>::find(T1 key){
+
+    return find(key,begin());
 }
 
 #endif
